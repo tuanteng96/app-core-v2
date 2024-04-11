602f4926a9c1b2a0db9dccbd41bed3054e7b2ee7
tuanteng96 <tuantengyn@gmail.com>
2024-04-11 11:21:13 +0700

    first commit

---
+++ src/pages/user/WheelSpin.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { Page, Link, Navbar } from "framework7-react";
+import IframeWheel from "./components/IframeWheel";
+
+export default class extends React.Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+  componentDidMount() {
+  }
+
+  render() {
+    return (
+      <Page name="wheel" noToolbar>
+        <Navbar>
+          <div className="page-navbar">
+            <div className="page-navbar__back">
+              <Link onClick={() => this.$f7router.back("/", { force: true })}>
+                <i className="las la-angle-left"></i>
+              </Link>
+            </div>
+            <div className="page-navbar__title">
+              <span className="title">Vòng quay may mắn</span>
+            </div>
+          </div>
+        </Navbar>
+
+        <div className="h-100">
+          <IframeWheel f7={this.$f7} params={this.$f7route.params}/>
+        </div>
+      </Page>
+    );
+  }
+}
---
+++ src/pages/user/components/IframeWheel.jsx
@@ -0,0 +1,40 @@
+import React, { useEffect, useRef, useState } from "react";
+import Dom7 from "dom7";
+import IframeComm from "react-iframe-comm";
+import { getStockIDStorage, getUser } from "../../../constants/user";
+import { SERVER_APP } from "../../../constants/config";
+
+window.Info = {
+  User: getUser(),
+  Stocks: [],
+  CrStockID: getStockIDStorage(),
+};
+
+function IframeWheel({ f7, params }) {
+  useEffect(() => {
+    var $ = Dom7;
+    if ($(".dialog-preloader").length === 0) {
+      f7.dialog.preloader("Đang tải vòng quay ... ");
+    }
+  }, []);
+
+  return (
+    <IframeComm
+      attributes={{
+        src: `${SERVER_APP}/vong-quay-may-man?v=${new Date().valueOf()}&DepartmentID=${params?.DepartmentID}&EndDate=${params?.EndDate}`,
+        width: "100%",
+        height: "100%",
+        frameBorder: 0,
+        display: "block",
+      }}
+      postMessageData={JSON.stringify({
+        Info: getUser(),
+      })}
+      handleReady={() => {
+        f7.dialog.close();
+      }}
+    />
+  );
+}
+
+export default IframeWheel;
